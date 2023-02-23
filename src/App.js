714a1bab2f262e6630d5ba27dc77901fc718b864
Nita1001 <devinedevops@gmail.com>
2023-02-24 01:25:20 +0200

    Guess the Anime

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Card from './components/Card.component'
+import edward from './Images/EdwardElric.jpg'
+import killua from './Images/KilluaZoldyck.avif'
+import roronoa from './Images/Roronoa_Zoro1.webp'
+import kz from './Images/KZ.webp'
+import saturo from './Images/SaturoGojo.avif'
+
+
 import './App.css';
 
 function App() {
+
+  const [image, setImage] = useState(null);
+  const images = [edward, killua, roronoa, kz, saturo];
+
+  useEffect(()=>{
+    const item = images[Math.floor(Math.random()*images.length)];
+    setImage(item);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='container'>
+
+          <h3>Guess the Anime</h3>
+
+     
+         <Card img={image}></Card>
+      
+
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Card.component.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = ({img}) => {
+
+
+  return (
+    <div>
+        <img src={img}></img>
+    </div>
+  )
+
+
+}
+
+export default Card
